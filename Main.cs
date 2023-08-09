7d84cff194140abbab049a60e13d05cdfc0abfd1
Eloá Mello <eloamello126@gmail.com>
2023-08-09 17:47:24 -0300

    :tada: feat: first commit

---
+++ Main.cs
@@ -0,0 +1,30 @@
+﻿namespace CsharpChallenges
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------- Bem-vindo(a) --------");
+            Console.WriteLine("Escolha a aplicação a ser executada:  ");
+            Console.WriteLine("1 - Criptografia e descriptografia de arquivos utilizando Cifra de César");
+            Console.WriteLine("2 - ");
+
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("----- Cifra de César -----");
+                    CaesarCipher caesarCipher = new CaesarCipher();
+                    caesarCipher.Run();
+                    break;
+                case "2":
+                    Console.WriteLine("----- xxx -----");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
+    }
+}
