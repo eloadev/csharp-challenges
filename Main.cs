97605da42f1e3d40ac761d2b124fc76067fbc9b6
Eloá Mello <eloamello126@gmail.com>
2023-08-10 15:40:17 -0300

    refactor: remove unused option from main and refactor a bit the code

--- Main.cs
+++ Main.cs
@@ -1,30 +1,41 @@
-﻿namespace CsharpChallenges
+﻿using CsharpChallenges.Challenges;
+
+namespace CsharpChallenges
 {
     class MainClass
     {
         static void Main(string[] args)
         {
             Console.WriteLine("-------- Bem-vindo(a) --------");
-            Console.WriteLine("Escolha a aplicação a ser executada:  ");
-            Console.WriteLine("1 - Criptografia e descriptografia de arquivos utilizando Cifra de César");
-            Console.WriteLine("2 - ");
 
-            var option = Console.ReadLine();
-
-            switch (option)
+            while (true)
             {
-                case "1":
-                    Console.WriteLine("----- Cifra de César -----");
-                    CaesarCipher caesarCipher = new CaesarCipher();
-                    caesarCipher.Run();
-                    break;
-                case "2":
-                    Console.WriteLine("----- xxx -----");
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida");
-                    break;
+                Console.WriteLine("Escolha a aplicação a ser executada:  ");
+                Console.WriteLine("1 - Cifrar e decifrar arquivos utilizando Cifra de César");
+                Console.WriteLine("2 - ");
+                Console.WriteLine("3 - Fechar programa");
+
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("----- Cifra de César -----");
+                        CaesarCipher caesarCipher = new();
+                        caesarCipher.Run();
+                        break;
+                    case "2":
+                        Console.WriteLine("----- xxx -----");
+                        break;
+                    case "3":
+                        Console.WriteLine("----- Fechando programa -----");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
             }
+            
         }
     }
 }
