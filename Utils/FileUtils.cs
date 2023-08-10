ddbc167ad22d3c1039b689a5c8408716832af1d6
Eloá Mello <eloamello126@gmail.com>
2023-08-10 15:39:26 -0300

    feat: add file related class

---
+++ Utils/FileUtils.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CsharpChallenges.Utils
+{
+    internal class FileUtils
+    {
+        public FileUtils()
+        {
+        }
+
+        internal static string ReadFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        internal static void WriteFile(string filePath, string text)
+        {
+            File.WriteAllText(filePath, text);
+        }
+    }
+}
