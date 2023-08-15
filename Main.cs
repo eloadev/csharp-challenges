ee4897f4a89642117d119a7af4d7b445c55caec6
Eloá Mello <eloamello126@gmail.com>
2023-08-15 14:57:00 -0300

    feat: new option for base64 challenge on menu

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 ﻿using CsharpChallenges.Challenges.CaesarCipher;
+using CsharpChallenges.Challenges.Base64;
 
 namespace CsharpChallenges
 {
@@ -12,6 +13,7 @@ namespace CsharpChallenges
             {
                 Console.WriteLine("Escolha a aplicação a ser executada:  ");
                 Console.WriteLine("1 - Cifrar e decifrar arquivos utilizando Cifra de César");
+                Console.WriteLine("2 - Codificar e decodificar arquivos utilizando Base64");
                 Console.WriteLine("3 - Fechar programa");
 
                 var option = Console.ReadLine();
@@ -21,6 +23,10 @@ namespace CsharpChallenges
                     case "1":
                         Console.WriteLine("----- Cifra de César -----");
                         new CaesarCipher().Run();
+                        break;
+                    case "2":
+                        Console.WriteLine("----- Base64 -----");
+                        new Base64().Run();
                         break;
                     case "3":
                         Console.WriteLine("----- Fechando programa -----");
